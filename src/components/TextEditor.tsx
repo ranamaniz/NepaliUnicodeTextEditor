463b410f9453bfc84fd62c12a6c3952717c48240
manish <alertmanishrana@gmail.com>
2024-09-15 11:08:00 +0545

    add english to nepali char conversion

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { createEditor } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
+import { Editable, Slate, withReact } from "slate-react";
 
 import { BaseEditor, Descendant } from "slate";
 import { ReactEditor } from "slate-react";
-import { Button } from "react-bootstrap";
+import { preetiCharMap } from "../constants";
 
 type CustomElement = { type: "paragraph"; children: CustomText[] };
 type CustomText = { text: string };
@@ -25,21 +25,63 @@ const initialValue: Descendant[] = [
 ];
 
 const TextEditor = () => {
+  const [isShiftActive, setIsShiftActive] = useState(false);
+
   const [editor] = useState(() => withReact(createEditor()));
 
+  const handlePreetiCharMap = (key: string) => {
+    if (isShiftActive) {
+      const uKey = key.toUpperCase();
+
+      console.log(uKey);
+
+      return key === "Shift" ? "" : preetiCharMap[uKey] || uKey;
+    }
+
+    return key === "Shift" ? "" : preetiCharMap[key] || key;
+  };
+
   const handleKeyDown = (e: React.KeyboardEvent) => {
+    console.log("e.key", e.key);
+
+    if (e.shiftKey) {
+      setIsShiftActive(true);
+    }
+
+    const isActionKey =
+      e.ctrlKey ||
+      e.altKey ||
+      e.metaKey ||
+      e.key === " " ||
+      e.key === "Backspace" ||
+      e.key === "Tab";
+
+    if (isActionKey) {
+      return;
+    }
+
+    e.preventDefault();
+
     const key = e.key;
-    if (key === "&") {
-      e.preventDefault();
-      editor.insertText("and");
+
+    const preetiText = handlePreetiCharMap(key);
+
+    editor.insertText(preetiText);
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent) => {
+    if (e.shiftKey) {
+      setIsShiftActive(false);
     }
   };
+
   return (
     <Slate editor={editor} initialValue={initialValue}>
       <Editable
         placeholder="Type something..."
         className="textEditor__textarea"
         onKeyDown={(e) => handleKeyDown(e)}
+        onKeyUp={(e) => handleKeyUp(e)}
       />
     </Slate>
   );
